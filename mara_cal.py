0431cec8987c16dc15a9528d35d094f838fe90b1
냥말 <37324983+nyangmal@users.noreply.github.com>
2021-11-24 12:54:32 +0900

    2021.11.24
    재료 추가 방식 및 폰트 가독성 수정

--- mara_cal.py
+++ mara_cal.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import font
 from typing import List
 
 # 재료 이미지 변경 함수
@@ -20,7 +21,32 @@ def addIngredient():
     temp = lb.curselection()[0]
     name = ing_name[temp]
     kcal = ing.get(name)
-    tree.insert("", END, values=(name, "100g", kcal))
+    addKcal(kcal)
+    ingExist(temp, name, kcal)
+
+
+# 추가한 칼로리 더하기
+def addKcal(kcal):
+    global total_kcal
+    total_kcal += kcal
+    kcal_label.configure(text=("총 칼로리 : " + str(total_kcal)))
+
+
+# 재료 추가 전 존재여부 체크
+def ingExist(temp, name, kcal):
+    if tree.exists(temp):
+        amout_arr[temp] += 1
+        tree.item(temp, values=(name, unitSelector(temp), amout_arr[temp] * kcal))
+    else:
+        tree.insert("", END, iid=temp, values=(name, unitSelector(temp), kcal))
+
+
+# 재료 단위 결정
+def unitSelector(temp):
+    if temp in count_arr:
+        return str(amout_arr[temp]) + " 개"
+    else:
+        return str(amout_arr[temp]) + "00g"
 
 
 # 윈도우 생성
@@ -52,21 +78,35 @@ ing = {
     "팽이버섯": 22,
     "표고버섯": 16,
 }
-ing_name = list(ing.keys())
-current_img = PhotoImage(file="img/0.png")
+total_kcal = 0  # 총 칼로리의 합
+count_arr = [1, 3, 7, 8, 9, 14, 17, 18, 19]  # 단위가 개수인 재료들
+amout_arr = []  # 재료 양 저장하는 배열
+ing_name = list(ing.keys())  # 재료 이름들의 리스트
+init_img = PhotoImage(file="img/0.png")
+for i in range(len(ing_name)):
+    amout_arr.append(1)
 
 # 재료 사진칸 생성
 img_label = Label(
-    win, image=current_img, borderwidth=1, relief="ridge", background="white"
+    win, image=init_img, borderwidth=1, relief="groove", background="white"
 )
 img_label.place(x=0, y=0, width=900, height=500)
 
 # 재료 선택 안내창 생성
-ing_text = Label(win, text="음식 선택", background="white", borderwidth=1, relief="ridge")
+menu_font = ("Malgun Gothic", 12, "bold")
+ing_text = Label(
+    win,
+    text="음식 선택",
+    background="white",
+    borderwidth=1,
+    relief="groove",
+    font=menu_font,
+)
 ing_text.place(x=900, y=0, width=300, height=30)
 
 # 재료 선택창 생성
-lb = Listbox(win)
+lb_font = ("Malgun Gothic", 16)
+lb = Listbox(win, cursor="heart", font=lb_font)
 for i in range(len(ing_name)):
     lb.insert(i, ing_name[i])
 lb.select_set(0)
@@ -74,7 +114,7 @@ lb.place(x=900, y=30, width=300, height=440)
 lb.bind("<ButtonRelease>", lbClick)
 
 # 재료 추가 버튼 생성
-ing_add = Button(win, text="추가", command=addIngredient)
+ing_add = Button(win, text="추가", command=addIngredient, font=menu_font)
 ing_add.place(x=900, y=470, width=300, height=30)
 
 # 추가된 재료란 = 게시판
@@ -90,14 +130,25 @@ tree.heading(3, text="칼로리", anchor=CENTER)
 tree.place(x=0, y=500, width=1200, height=300)
 
 # 총 칼로리
+menu_font2 = ("Malgun Gothic", 20, "bold")
 kcal_label = Label(
-    win, text="총 칼로리 :", background="white", borderwidth=1, relief="ridge"
+    win,
+    text=("총 칼로리 : " + str(total_kcal)),
+    background="white",
+    borderwidth=1,
+    relief="groove",
+    font=menu_font2,
 )
 kcal_label.place(x=0, y=800, width=600, height=100)
 
 # 운동 시간
 exercise_label = Label(
-    win, text="소모 시간 : ", background="white", borderwidth=1, relief="ridge"
+    win,
+    text="소모 시간 : ",
+    background="white",
+    borderwidth=1,
+    relief="groove",
+    font=menu_font2,
 )
 exercise_label.place(x=600, y=800, width=600, height=100)
 
